ca9705a63f46d80a0ae54da2161d56562c54a9c7
alejandro <alejandrober96@gmail.com>
2023-02-23 13:17:47 -0500

    filter implemented on clearAll

--- src/clearAllCompleted.js
+++ src/clearAllCompleted.js
@@ -1,11 +1,20 @@
 import remove from './remove.js';
 
+
 export default () => {
   const checkboxs = document.querySelectorAll('.checkbox');
-  checkboxs.forEach((box) => {
-    if (box.checked === true) {
-      remove(box.nextSibling.value);
-      box.parentElement.remove();
-    }
-  });
+  console.log(checkboxs)
+  const arr = Array.from(checkboxs)
+  const checkboxCheck = arr.filter((check) => check.checked === true);
+  // checkboxs.forEach((box) => {
+  //   if (box.checked === true) {
+  //     remove(box.nextSibling.value);
+  //     box.parentElement.remove();
+  //   }
+  // });
+checkboxCheck.forEach((cheked) => {
+  remove(cheked.nextSibling.value)
+  cheked.parentElement.remove();
+})
+  console.log(checkboxCheck[0].nextSibling.value)
 };
