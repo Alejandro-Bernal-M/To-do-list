52eae0607739ed6f3bbc48b22b9567353f0f439a
alejandro <alejandrober96@gmail.com>
2023-02-23 13:19:07 -0500

    linters

--- src/clearAllCompleted.js
+++ src/clearAllCompleted.js
@@ -1,20 +1,11 @@
 import remove from './remove.js';
 
-
 export default () => {
   const checkboxs = document.querySelectorAll('.checkbox');
-  console.log(checkboxs)
-  const arr = Array.from(checkboxs)
+  const arr = Array.from(checkboxs);
   const checkboxCheck = arr.filter((check) => check.checked === true);
-  // checkboxs.forEach((box) => {
-  //   if (box.checked === true) {
-  //     remove(box.nextSibling.value);
-  //     box.parentElement.remove();
-  //   }
-  // });
-checkboxCheck.forEach((cheked) => {
-  remove(cheked.nextSibling.value)
-  cheked.parentElement.remove();
-})
-  console.log(checkboxCheck[0].nextSibling.value)
+  checkboxCheck.forEach((cheked) => {
+    remove(cheked.nextSibling.value);
+    cheked.parentElement.remove();
+  });
 };
