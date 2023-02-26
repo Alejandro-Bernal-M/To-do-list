6852c0d1d5b963e0a0bb21b38c582935a307f8ff
alejandro <alejandrober96@gmail.com>
2023-02-25 21:23:08 -0500

    dragging v1

--- src/dragging.js
+++ src/dragging.js
@@ -40,32 +40,74 @@ function getElementBefore(y) {
 
 export default () => {
   const draggables = document.querySelectorAll('.toDrag:not(.trash-can)');
+  
   const holder = document.querySelector('.ul-to-do');
   let dragged;
+  draggables.forEach((button) => {
+    button.addEventListener('mousedown', () => {
+      const items = document.querySelectorAll('.task-item');
+      const arrOfPositions = [];
+      items.forEach((item) => {
+        arrOfPositions.push(item.getBoundingClientRect().y);
+      });
+      console.log('one', arrOfPositions)
+      button.parentElement.classList.add('dragging')
+      button.draggable = false;
+      const nodeToDrag = button.parentNode;
+      button.parentNode.style.backgroundColor = '#777';
+      button.parentNode.childNodes[1].style.backgroundColor = '#777';
+      button.parentNode.draggable = true;
+      nodeToDrag.addEventListener('dragstart', () => {
+        dragged = nodeToDrag;
+      });
+      let lastMousePosition;
+      holder.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        lastMousePosition = e.clientY;
+        const where = arrOfPositions.findIndex((position) => position > lastMousePosition);
+        // if(where === -1) {
 
-  draggables.forEach((draggable) => {
-    draggable.addEventListener('dragstart', (event) => {
-      draggable.parentElement.classList.add('dragging');
-      draggable.parentElement.draggable = true;
-      draggable.parentElement.style.backgroundColor = '#777';
-      draggable.parentElement.childNodes[1].style.backgroundColor = '#777';
-      dragged = event.target.parentElement;
-    });
-    draggable.addEventListener('dragend', () => {
-      draggable.parentElement.classList.remove('dragging');
-      draggable.parentElement.draggable = false;
-    });
-  });
+        // }
+        holder.insertBefore(dragged, items[where]);
+      });
+      nodeToDrag.addEventListener('dragend', () => {
 
-  holder.addEventListener('dragover', (e) => {
-    e.preventDefault();
-    const elementBefore = getElementBefore(e.clientY);
-    if (elementBefore === null || elementBefore === undefined) {
-      holder.appendChild(dragged);
-    } else {
-      holder.insertBefore(dragged, elementBefore);
-    }
-  });
+      });
+    })
+    button.addEventListener('mouseleave', () => {
+      button.parentElement.classList.remove('dragging');
+      button.parentNode.draggable = false;
+    })
+    button.addEventListener('mouseup', () => {
+      button.parentElement.classList.remove('dragging');
+      button.parentNode.style.backgroundColor = '#fff';
+      button.parentNode.childNodes[1].style.backgroundColor = '#fff';
+    })
+  })
+
+  // draggables.forEach((draggable) => {
+  //   draggable.addEventListener('dragstart', (event) => {
+  //     draggable.parentElement.classList.add('dragging');
+  //     draggable.parentElement.draggable = true;
+  //     draggable.parentElement.style.backgroundColor = '#777';
+  //     draggable.parentElement.childNodes[1].style.backgroundColor = '#777';
+  //     dragged = event.target.parentElement;
+  //   });
+  //   draggable.addEventListener('dragend', () => {
+  //     draggable.parentElement.classList.remove('dragging');
+  //     draggable.parentElement.draggable = false;
+  //   });
+  // });
+
+  // holder.addEventListener('dragover', (e) => {
+  //    e.preventDefault();
+  // const elementBefore = getElementBefore(e.clientY);
+  // if (elementBefore === null || elementBefore === undefined) {
+  //   holder.appendChild(dragged);
+  // } else {
+  //   holder.insertBefore(dragged, elementBefore);
+  // }
+  //});
 
   holder.addEventListener('drop', (event) => {
     event.preventDefault();
